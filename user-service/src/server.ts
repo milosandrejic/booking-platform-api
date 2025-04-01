5f60b8aeb46deabfdd959ca657356ac445a4362c
Milos Andrejic <ma@retrocode.com>
2025-04-01 15:40:24 +0200

    migration script whit dynamic filename params, more initial setup tweaks

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import "dotenv/config"
+import {dataSource} from "src/db/config"
 
 const app: Express = express();
+
+app.use(express.json())
+
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello!")
+dataSource.initialize();
 });
 
 app.listen(port, () => {

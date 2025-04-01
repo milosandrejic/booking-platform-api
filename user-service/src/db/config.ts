4d9090b331f0f9c35082719eddda21ca2bf22647
Milos Andrejic <ma@retrocode.com>
2025-04-01 14:39:36 +0200

    split local and docker env

--- user-service/src/db/config.ts
+++ user-service/src/db/config.ts
@@ -2,10 +2,10 @@ import "dotenv/config"
 
 import { DataSource } from "typeorm";
 
-export const UsersDataSource = new DataSource({
+export const dataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? "9432", 10),
+  port: parseInt(process.env.DB_PORT ?? "5432", 10),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,

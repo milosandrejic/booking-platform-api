d3915d0216eb0c2f0fc68e31e9a33716dbcc55a7
Milos Andrejic <ma@retrocode.com>
2025-04-01 09:16:04 +0200

    initial sql, return to npm install in dockerfile, tsconfig paths

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,8 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import "dotenv/config"
-import "reflect-metadata";
 
-import Profile from "@/model/profile"
+import Profile from "src/model/profile"
 
 const app: Express = express();
 const port = process.env.PORT;

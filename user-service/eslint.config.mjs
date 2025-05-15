20e291df42cd99a9808ecfd8a42b7e5afb1ea356
Milos Andrejic <milosandrejic1993@gmail.com>
2025-05-15 13:38:27 +0200

    eslint ignore

--- user-service/eslint.config.mjs
+++ user-service/eslint.config.mjs
@@ -1,37 +1,38 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import {defineConfig} from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
 import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
+  globalIgnores(["eslint.config.mjs", "node_modules"]),
   {
-    "files": ["src/*.{js,mjs,cjs,ts}"],
-    "plugins": {
+    files: ["./src/*.{js,mjs,cjs,ts}"],
+    plugins: {
       js,
       "@stylistic": stylistic
     },
-    "extends": ["js/recommended"]
+    extends: ["js/recommended"]
   },
   {
-    "files": ["**/*.ts"],
-    "languageOptions": {"sourceType": "commonjs"}
+    files: ["**/*.ts"],
+    languageOptions: {sourceType: "commonjs"}
   },
   {
-    "files": ["**/*.{js,mjs,cjs,ts}"],
-    "languageOptions": {"globals": globals.browser}
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {globals: globals.browser}
   },
   tseslint.configs.recommended,
   stylistic.configs.all,
   {
-    "rules": {
-      "semi": "error",
+    rules: {
+      semi: "error",
       "@stylistic/indent": ["warn", 2],
       "@stylistic/array-element-newline": [
         "warn",
         {
-          "multiline": true,
-          "minItems": 3
+          multiline: true,
+          minItems: 4
         }
       ],
       "@stylistic/quote-props": ["warn", "as-needed"]

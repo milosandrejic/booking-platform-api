8979ffdbba929a917e4154479719f77a5d8dcb32
Milos Andrejic <milosandrejic1993@gmail.com>
2025-05-26 16:34:43 +0200

    Revert "automatic db creation on the first container run"
    This reverts commit 629018272876ce8627cf191d8758f5ca3b46e1c3.

--- postgres-init/initial.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER booking WITH PASSWORD 'yzPV225HeSWv';
-CREATE DATABASE "booking-users" OWNER booking;
-CREATE DATABASE "booking-properties" OWNER booking;
-GRANT ALL PRIVILEGES ON DATABASE "booking-users" TO booking;
-GRANT ALL PRIVILEGES ON DATABASE "booking-properties" TO booking;
